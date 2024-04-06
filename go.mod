bb3c7812f99de0931fb11dd052e1382147b658d0
QuentinN42 <quentin@lieumont.fr>
2024-04-06 20:03:43 +0200

    feat: working poc
    Signed-off-by: QuentinN42 <quentin@lieumont.fr>

--- go.mod
+++ go.mod
@@ -2,6 +2,12 @@ module github.com/QuentinN42/xztester
 
 go 1.22.1
 
-require github.com/sagikazarmark/slog-shim v0.1.0
+require (
+	github.com/sagikazarmark/slog-shim v0.1.0
+	golang.org/x/crypto v0.22.0
+)
 
-require golang.org/x/exp v0.0.0-20230905200255-921286631fa9 // indirect
+require (
+	golang.org/x/exp v0.0.0-20230905200255-921286631fa9 // indirect
+	golang.org/x/sys v0.19.0 // indirect
+)
--- pkg/tester/main.go
+++ pkg/tester/main.go
@@ -2,9 +2,63 @@ package tester
 
 import (
 	"context"
+	"io"
+	"net"
 	"time"
+
+	"golang.org/x/crypto/ssh"
 )
 
-func Test(ctx context.Context, addr string) (time.Duration, time.Duration, error) {
-	return time.Second, time.Second, nil
+const toCatchValue = "37355060-8ef2-47c3-833d-c78c3663cd66"
+const toCatchValueRes = "ssh: handshake failed: " + toCatchValue
+
+type toCatch struct{}
+type NotCatched struct{}
+
+func (e toCatch) Error() string    { return toCatchValue }
+func (e NotCatched) Error() string { return "HostKeyCallback not called" }
+
+func Test(ctx context.Context, addr string) (handshake time.Duration, readkey time.Duration, err error) {
+	xz := &xzTester{}
+	config := &ssh.ClientConfig{
+		User: "root",
+		Auth: []ssh.AuthMethod{
+			ssh.PublicKeys(xz),
+		},
+		HostKeyCallback: xz.HostKeyCallback,
+	}
+
+	start := time.Now()
+	conn, err := net.DialTimeout("tcp", addr, config.Timeout)
+	handshake = time.Since(start)
+
+	if err != nil {
+		return handshake, readkey, err
+	}
+
+	start = time.Now()
+	_, _, _, err = ssh.NewClientConn(conn, addr, config)
+	readkey = time.Since(start)
+
+	if err == nil {
+		return handshake, readkey, NotCatched{}
+	}
+	if err.Error() != toCatchValueRes {
+		return handshake, readkey, err
+	}
+	return handshake, readkey, nil
+}
+
+type xzTester struct{}
+
+func (s *xzTester) PublicKey() ssh.PublicKey {
+	return nil // will never be called
+}
+
+func (s *xzTester) Sign(rand io.Reader, data []byte) (*ssh.Signature, error) {
+	return nil, nil // will never be called
+}
+
+func (s *xzTester) HostKeyCallback(_ string, _ net.Addr, key ssh.PublicKey) error {
+	return toCatch{}
 }
